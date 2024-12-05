9dc3251b6c88fd095342cc04533a8235073dc20f
Cullen Sharp <cullen.sharp100@gmail.com>
2024-12-05 01:34:42 -0800

    Begin writing src

---
+++ code/lib/temp_humid_tree/temp_humid_tree.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include "tempHumidtree.h"
+
+TempHumidTreePtr_t createTempHumidTree(void) {
+
+}
+
+BSTNodePtr_t insert(TempHumidTreePtr_t tree, DataItem_t info) {
+
+}
+
+BSTNodePtr_t search(TempHumidTreePtr_t tree, time_t timestamp) {
+
+}
+
+void inorder(TempHumidTreePtr_t tree) {
+    
+}
