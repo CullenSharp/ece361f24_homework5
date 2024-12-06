b1a13ff17de1e9ea42b19cbeb253adc8e8ef9c02
Cullen Sharp <cullen.sharp100@gmail.com>
2024-12-05 23:00:59 -0800

    Add main input loop

--- code/main.c
+++ code/main.c
@@ -6,7 +6,7 @@
 #include <errno.h>
 #include "main.h"
 #include "lib/iom361_r2/iom361_r2.h"
-  
+
 // global variables
 uint32_t* io_base;
 
@@ -23,7 +23,11 @@ static void shuffle(DataItemPtr_t readings) {
 }
 
 int main() {
-    int rtn_code;
+    int rtn_code, i;
+    int secs, mins, hour, mday, mon, year;
+    char date_time_buf[32] = {'\0'};
+    char ch;
+    struct tm date_time_str;
     TempHumidTreePtr_t tree = createTempHumidTree();
 
     // Init iom
@@ -53,8 +57,37 @@ int main() {
     printf("\n");
 
     populateBST(tree);
+
+    while(1) {
+        i = 0;
+        printf("Enter timestamp: ");
+        while ((ch = getchar()) != '\n')
+            date_time_buf[i++] = ch;
+
+        date_time_buf[i] = '\0';
+
+        if (date_time_buf[0] == '\0')
+            break;
+        
+        sscanf(date_time_buf,
+            " %2d / %2d / %4d %2d : %2d : %2d",
+            &mon, &mday, &year, &hour, &mins, &secs);
+        
+        date_time_str.tm_year = year - 1900;
+        date_time_str.tm_mon = mon - 1;
+        date_time_str.tm_mday = mday;
+        date_time_str.tm_hour = hour - 7;
+        date_time_str.tm_min = mins;
+        date_time_str.tm_sec = secs;
+        date_time_str.tm_isdst = -1;
+        search(tree, mktime(&date_time_str));
+    }
+
+    printf("Printing the whole table\n");
     inorder(tree);
     destroy(tree);
+
+    printf("Thank you for using my program!\n");
     return 0;
 }
 
